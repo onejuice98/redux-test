435d1a3fe961656473105f4a6609ad529c097bcc
onejuice98 <waterpurifier@khu.ac.kr>
2023-07-10 00:00:38 +0900

    router, login & main

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import Counter from "./components/Counter";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/intex";
 
 function App() {
   return (
     // Provider를 통해서 store 저장
     // store는 저장소이며 여러 slice로 이루어져 있다.
     <Provider store={store}>
-      <Counter />
+      <BrowserRouter>
+        <Link to={"/"}>Main</Link>
+        <Link to={"/login"}>Login</Link>
+        <Routes>
+          <Route path={"/"} element={<Counter />} />
+          <Route path={"/login"} element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
